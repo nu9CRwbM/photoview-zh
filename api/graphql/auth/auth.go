420ef126b7e0f4920b766041363f06bff929de5c
nu9CRwbM <pf.yang1991@gmail.com>
2023-08-25 16:46:49 +0800

    重构auth.go

--- api/graphql/auth/auth.go
+++ api/graphql/auth/auth.go
@@ -15,44 +15,43 @@ import (
 
 var ErrUnauthorized = errors.New("unauthorized")
 
-// A private key for context that only this package can access. This is important
-// to prevent collisions between different context uses
 var userCtxKey = &contextKey{"user"}
 
 type contextKey struct {
 	name string
 }
 
+// Extracts user from token and adds it to the context
+func addUserFromTokenToContext(r *http.Request, db *gorm.DB) (*http.Request, error) {
+	tokenCookie, err := r.Cookie("auth-token")
+	if err != nil {
+		log.Println("Did not find auth-token cookie")
+		return r, nil
+	}
+
+	user, err := dataloader.For(r.Context()).UserFromAccessToken.Load(tokenCookie.Value)
+	if err != nil {
+		log.Printf("Invalid token: %s\n", err)
+		return nil, ErrUnauthorized
+	}
+
+	ctx := context.WithValue(r.Context(), userCtxKey, user)
+	return r.WithContext(ctx), nil
+}
+
 // Middleware decodes the share session cookie and packs the session into context
 func Middleware(db *gorm.DB) func(http.Handler) http.Handler {
 	return func(next http.Handler) http.Handler {
 		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-
-			if tokenCookie, err := r.Cookie("auth-token"); err == nil {
-				user, err := dataloader.For(r.Context()).UserFromAccessToken.Load(tokenCookie.Value)
-				// user, err := models.VerifyTokenAndGetUser(db, tokenCookie.Value)
-				if err != nil {
-					log.Printf("Invalid token: %s\n", err)
-					http.Error(w, "invalid authorization token", http.StatusForbidden)
-					return
-				}
-
-				// put it in context
-				ctx := AddUserToContext(r.Context(), user)
-
-				// and call the next with our new context
-				r = r.WithContext(ctx)
-			} else {
-				log.Println("Did not find auth-token cookie")
+			r, err := addUserFromTokenToContext(r, db)
+			if err != nil {
+				http.Error(w, "invalid authorization token", http.StatusForbidden)
+				return
 			}
 
 			next.ServeHTTP(w, r)
 		})
 	}
-}
-
-func AddUserToContext(ctx context.Context, user *models.User) context.Context {
-	return context.WithValue(ctx, userCtxKey, user)
 }
 
 func TokenFromBearer(bearer *string) (*string, error) {
@@ -87,16 +86,13 @@ func AuthWebsocketInit(db *gorm.DB) func(context.Context, handler.InitPayload) (
 		}
 
 		user, err := dataloader.For(ctx).UserFromAccessToken.Load(*token)
-		// user, err := models.VerifyTokenAndGetUser(db, *token)
 		if err != nil {
 			log.Printf("Invalid token in websocket: %s\n", err)
 			return nil, errors.New("invalid authorization token")
 		}
 
-		// put it in context
 		userCtx := context.WithValue(ctx, userCtxKey, user)
 
-		// and return it so the resolvers can see it
 		return userCtx, nil
 	}
 }
